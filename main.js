22ead15136a38b948147df4152ffb79b466f137a
Sidopjescherm <hermussidney@gmail.com>
2024-12-12 13:46:07 +0100

    Fix 6 Benaming
    Voor documentatie vind het belangrijk dat ik een duidelijke beschrijving geef bij welke animatie bij welke link hoort en welke Javascript code bij welke link hoort. Dat heb ik deze Fix daarbij ook gedaan.

--- main.js
+++ main.js
@@ -3,62 +3,38 @@
    Als je op het 12e linkje klikt (“Interaction”), springt deze omhoog
 */
 
-// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
+/*Hier is het event dat bij de 'Interaction' link hoort */
 let interaction = document.querySelector('a:nth-of-type(12)')
 
-// Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
-// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
 interaction.addEventListener('click', jumpHandler)
 
-// Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
-// _niet_ meteen bij het laden van de pagina aangeroepen
 function jumpHandler() {
 
-  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
   interaction.classList.toggle('jump')
 }
 
-// Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
 interaction.addEventListener('animationend', jumpHandler)
 
-
-// Ga zelf verder met de overige elementen, aan de hand van de instructies
-// Maak bijvoorbeeld een bibber animatie als je op iets klikt
-
-
-// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
-let bibberlink = document.querySelector('a:nth-of-type(2)')
-
-// Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
-// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-bibberlink.addEventListener('mouseover', bibberHandler)
-
-// Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
-// _niet_ meteen bij het laden van de pagina aangeroepen
-function bibberHandler() {
-
-  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  bibberlink.classList.toggle('bibberlink')
-}
-
-
-
-
-// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
+/*Hier is het event dat hoort bij de 'Frontend' link*/
 let swipefade = document.querySelector('a:nth-of-type(1)')
 
-// Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
-// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
 swipefade.addEventListener('mouseup', dissapear)
 
-// Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
-// _niet_ meteen bij het laden van de pagina aangeroepen
 function dissapear() {
-
-  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
   swipefade.classList.toggle('swipe')
 }
 
+/*Hier is het event dat bij de 'design' link hoort */
+let bibberlink = document.querySelector('a:nth-of-type(2)')
+
+bibberlink.addEventListener('mouseover', bibberHandler)
+
+function bibberHandler() {
+
+  bibberlink.classList.toggle('bibberlink')
+}
+
+/*Hier is het even dat hoort bij de '&' link*/ 
 let vergroten = document.querySelector('a:nth-of-type(3)')
 
 vergroten.addEventListener('click', vergroot)
@@ -67,6 +43,7 @@ function vergroot(){
   vergroten.classList.toggle('vergroten')
 }
 
+/*Hier is het event dat hoor bij de 'development' link*/ 
 let kleurencyclus = document.querySelector('a:nth-of-type(4)')
 
 kleurencyclus.addEventListener('dblclick', colorcycle)
@@ -75,6 +52,7 @@ function colorcycle(){
   kleurencyclus.classList.toggle('RGB')
 }
 
+/*Hier is het event die hoort bij de 'Sprint 5' link*/ 
 let stretchngo = document.querySelector('a:nth-of-type(5)')
 
 stretchngo.addEventListener('click', Sonic)
@@ -83,8 +61,17 @@ function Sonic(){
   stretchngo.classList.toggle('rekenren')
 }
 
-/*hier moet nog de Fix bounce button komen*/
+/*Hier is het event dat hoort bij de 'Fix' link*/
 
+let bouncing = document.querySelector('a:nth-of-type(6)')
+
+bouncing.addEventListener('mouseout', stuiter)
+
+function stuiter(event){
+  bouncing.classList.toggle('bounce')
+}
+
+/*Hier is het event die hoort bij de 'THE' link*/
 let glowing = document.querySelector('a:nth-of-type(7)')
 
 glowing.addEventListener('click', gloei)
